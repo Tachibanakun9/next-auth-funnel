d84ca47d66ea38ad5849887bcfcd94d41817d6e8
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-08-16 04:41:18 +0000

    navbar-worknotdone

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,47 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import UserButton from "./UserButton";
 import { Button } from "./ui/button";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+ 
 
-export default function NavBar() {
-  const session = useSession();
-  const user = session.data?.user;
 
+
+
+
+ function NavBar({ className }: { className?: string }) {
+  
+  
+  const [active, setActive] = useState<string | null>(null);
   return (
-    <header className="sticky top-0 bg-background px-3 shadow-sm">
-      <nav className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between gap-3">
-        <Link href="/" className="font-bold">
-          Next-Auth v5 Tutorial
+    <div
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+        <MenuItem setActive={setActive} active={active} item="home">
+        </MenuItem>
         </Link>
-        {user && <UserButton user={user} />}
-        {!user && session.status !== "loading" && <SignInButton />}
-      </nav>
-    </header>
-  );
+
+      
+        <MenuItem setActive={setActive} active={active} item="Products">
+        <HoveredLink href="/products">Our Products</HoveredLink>
+        </MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="Blog">
+        </MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="Contact-us">
+        </MenuItem>
+
+      </Menu>
+      </div>
+  )
 }
 
-function SignInButton() {
-  return <Button onClick={() => signIn()}>Sign in</Button>;
-}
+
+
+export default NavBar
